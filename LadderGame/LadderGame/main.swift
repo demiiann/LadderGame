cdb2a33cd9b18fca4c1d6ab8c178e3117adc8407
demiiann <tkim0156@gmail.com>
2022-11-07 14:53:14 +0900

    step2

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
-
+print("게임에 참여할 인원수를 입력해주세요(2~8명) : ", terminator: "")
+let members = Int(readLine()!)!
+if members < 2 || members > 8 {
+    print("인원수는 최소 2명, 최대 8명까지 허용됩니다. 프로그램을 종료합니다.")
+    exit(0)
+}
